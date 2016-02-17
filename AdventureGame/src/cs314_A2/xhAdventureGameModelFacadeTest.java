119d63d951300f7af967fdf3e8b7002e17b99271
Xuehao(David) Hu <xuehaodavidhu@XuehaoDavids-MacBook-Pro.local>
2016-02-17 16:28:04 -0700

    -added in player movements tests

---
+++ AdventureGame/src/cs314_A2/xhAdventureGameModelFacadeTest.java
@@ -0,0 +1,16 @@
+package cs314_A2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class xhAdventureGameModelFacadeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		AdventureGameModelFacade model = new AdventureGameModelFacade();
+		assertTrue(,);
+	}
+
+}
