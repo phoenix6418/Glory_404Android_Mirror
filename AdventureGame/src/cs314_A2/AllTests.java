00b9ea37248a97a8e28dd183287b79640a6bf992
davsay <David@Davids-MacBook-Pro.local>
2016-02-17 16:31:38 -0700

    AllTests.java is used to run both the AdventureGameTest and the PlayerTest. More will be added as tests continue to be developed.

---
+++ AdventureGame/src/cs314_A2/AllTests.java
@@ -0,0 +1,11 @@
+package cs314_A2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PlayerTest.class, AdventureGameTest.class })
+public class AllTests {
+
+}
